d5f9a89a19ed794d644ab9d5a97ef3bf776dde47
danie <danie@Gomez.Dlink>
2025-05-20 16:52:50 -0500

    prueba p bellaka

---
+++ src/main/java/com/mycompany/tralalerito/Tralalerito.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tralalerito;
+
+/**
+ *
+ * @author danie
+ */
+public class Tralalerito {
+
+    public static void main(String[] args) {
+        System.out.println("tralala");
+    }
+}
