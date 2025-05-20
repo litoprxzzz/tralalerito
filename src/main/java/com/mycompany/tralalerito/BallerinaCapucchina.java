41446375561eb5db08a6a4abc58a44fe88a5e6f7
danie <danie@Gomez.Dlink>
2025-05-20 17:03:16 -0500

    agregué ballerina

---
+++ src/main/java/com/mycompany/tralalerito/BallerinaCapucchina.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tralalerito;
+
+/**
+ *
+ * @author danie
+ */
+public class BallerinaCapucchina {
+    public static void main(String[] args) {
+        System.out.print("mimimimi");
+    }
+}
